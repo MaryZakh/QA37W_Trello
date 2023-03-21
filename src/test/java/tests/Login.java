833ac3d4f3e79ad074444450b0cc6f46d590da9d
marii <z0559882272@gmail.com>
2023-03-21 15:58:55 +0200

    login

--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -1,10 +1,16 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Login extends TestBase{
+public class Login extends TestBase {
     @Test
-    public void login1(){
-
+    public void login1() {
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillINLoginForm("z0559882272@gmail.com", "vekmnbrb1020309000");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+        Assert.assertTrue(app.getUser().isLogged());
     }
 }
